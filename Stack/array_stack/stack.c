6f18e6a6d7faf3c0fcb8738ea08141e878fc0b9e
= <alliikochaki2@gmail.com>
2025-06-08 15:47:37 +0330

    feat: implement push, pop, and peek.

--- Stack/array_stack/stack.c
+++ Stack/array_stack/stack.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 #include "stack.h"
+
+#define ST_ERROR_VALUE INT_MIN
 
 Stack *st_create(size_t capacity)
 {
@@ -33,4 +36,55 @@ void st_destroy(Stack *stack)
 
     free(stack->array);
     free(stack);
+}
+
+void push(Stack *stack, int value)
+{
+    if (!stack)
+    {
+        fprintf(stderr, "push: null pointer stack.\n");
+        return;
+    }
+
+    if (stack->top == stack->capacity)
+    {
+        fprintf(stderr, "push: stack is full.\n");
+        return;
+    }
+
+    stack->array[stack->top++] = value;
+}
+
+int pop(Stack *stack)
+{
+    if (!stack)
+    {
+        fprintf(stderr, "pop: null pointer stack.\n");
+        return ST_ERROR_VALUE;
+    }
+
+    if (stack->top == 0)
+    {
+        fprintf(stderr, "pop: stack is empty.\n");
+        return ST_ERROR_VALUE;
+    }
+
+    return stack->array[--stack->top];
+}
+
+int peek(Stack *stack)
+{
+    if (!stack)
+    {
+        fprintf(stderr, "peek: null pointer stack.\n");
+        return ST_ERROR_VALUE;
+    }
+
+    if (stack->top == 0)
+    {
+        fprintf(stderr, "peek: stack is empty.\n");
+        return ST_ERROR_VALUE;
+    }
+
+    return stack->array[stack->top - 1];
 }
