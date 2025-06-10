2ee20ed7fce14fd756e4cac5834a56537f4d1979
= <alliikochaki2@gmail.com>
2025-06-10 16:49:09 +0330

    feat: add a simple useage of Stack

---
+++ Stack/linkedlist_stack/use.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main(void)
+{
+    Stack *stack = st_create();
+    // Creation failed
+    if (!stack)
+        return 1;
+
+    for (int i = 1; i <= 10; i++)
+        push(stack, i);
+
+    printf("\n---------------\n\n");
+
+    if (st_is_empty(stack) == 0)
+        printf("top element in stack: %d\n", peek(stack));
+
+    printf("\n---------------\n\n");
+
+    while (st_is_empty(stack) == 0)
+        printf("pop: %d\n", pop(stack));
+
+    printf("\n---------------\n\n");
+
+    if (st_is_empty(stack))
+        printf("stack is empty\n");
+
+    // Free allocated memory
+    st_destroy(stack);
+    return 0;
+}
