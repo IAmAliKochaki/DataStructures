29d81dd43e05e6ab1190d1459aaf6ba56ae593be
= <alliikochaki2@gmail.com>
2025-07-07 21:05:21 +0330

    feat: add bst_insert.

--- BinarySearchTree/bst.c
+++ BinarySearchTree/bst.c
@@ -37,3 +37,68 @@ void bst_destroy(BST *bst)
     free_tree_nodes(bst->root);
     free(bst);
 }
+
+static Node *create_node(int value)
+{
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (!new_node)
+        return NULL;
+
+    new_node->key = value;
+    new_node->left = new_node->right = new_node->parent = NULL;
+
+    return new_node;
+}
+
+static int insert_to_bst(Node *node, Node *new_node)
+{
+    if (new_node->key < node->key) // go to the left subtree
+    {
+        if (node->left == NULL)
+        {
+            node->left = new_node;
+            new_node->parent = node;
+            return 1;
+        }
+        insert_to_bst(node->left, new_node);
+    }
+
+    if (new_node->key > node->key) // go to the right subtree
+    {
+        if (node->right == NULL)
+        {
+            node->right = new_node;
+            new_node->parent = node;
+            return 1;
+        }
+        insert_to_bst(node->right, new_node);
+    }
+
+    return 0;
+}
+
+int bst_insert(BST *bst, int value)
+{
+    if (!bst)
+    {
+        fprintf(stderr, "bst_insert: null pointer binary search tree.\n");
+        return 0;
+    }
+
+    Node *new_node = create_node(value);
+    if (!new_node)
+    {
+        fprintf(stderr, "bst_insert: insert new value failed because allocation.\n");
+        return 0;
+    }
+
+    if (bst->root == NULL)
+        bst->root = new_node;
+    else if (insert_to_bst(bst->root, new_node) == 0)
+    {
+        fprintf(stderr, "bst_insert: insert new value failed because duplication.\n");
+        return 0;
+    }
+    bst->size++;
+    return 1;
+}
--- BinarySearchTree/bst.h
+++ BinarySearchTree/bst.h
@@ -20,8 +20,8 @@ typedef struct
 BST *bst_create();
 void bst_destroy(BST *bst);
 
-int bst_insert(const BST *bst, int value);
-int bst_remove(const BST *bst, int value);
+int bst_insert(BST *bst, int value); // Insert a value into the BST. Return 1 on success, 0 on failure.
+int bst_remove(BST *bst, int value); // Remove a value from the BST. Return 1 on success, 0 on failure.
 
 Node *bst_search(const BST *bst, int value); // Search for a value in the BST. Returns a pointer to the node if found, otherwise NULL.
 int bst_contains(const BST *bst, int value); // Return 1 if the value exists in the BST, 0 otherwise.
